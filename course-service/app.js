f1cfa3d997135900b5d5e8799cedf82ccb3cd7f7
Subhashree <subhabalasubramanian2@gmail.com>
2025-09-28 21:51:16 +0530

    feat(course-service): add course creation, enrollment, and recommendations

---
+++ course-service/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const logger = require('./middleware/logger');
+const courseRoutes = require('./routes/courseRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
+app.use(cors());
+app.use(express.json());
+app.use(logger); 
+app.use('/courses', courseRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Course Service running on port ${PORT}`));
